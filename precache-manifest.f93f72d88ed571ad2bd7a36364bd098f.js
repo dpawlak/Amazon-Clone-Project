ee2d0191eb1fb9d72f4bfd38d19be91634e854c8
Daniel Pawlak <daniel.pawlak.und@gmail.com>
2020-10-12 10:02:38 -0700

    Updates

---
+++ precache-manifest.f93f72d88ed571ad2bd7a36364bd098f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3210fdd1fff5aa308fff4d917120ee9a",
+    "url": "/dpawlak/Amazon-Clone-Project/index.html"
+  },
+  {
+    "revision": "442864ba3976f02a8d3b",
+    "url": "/dpawlak/Amazon-Clone-Project/static/css/main.21d84e04.chunk.css"
+  },
+  {
+    "revision": "7ec53c916a4e6bd26a38",
+    "url": "/dpawlak/Amazon-Clone-Project/static/js/2.5771220a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/dpawlak/Amazon-Clone-Project/static/js/2.5771220a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "442864ba3976f02a8d3b",
+    "url": "/dpawlak/Amazon-Clone-Project/static/js/main.03d7cd59.chunk.js"
+  },
+  {
+    "revision": "c292bb630c3b2f6f2660",
+    "url": "/dpawlak/Amazon-Clone-Project/static/js/runtime-main.8eb778d9.js"
+  }
+]);
