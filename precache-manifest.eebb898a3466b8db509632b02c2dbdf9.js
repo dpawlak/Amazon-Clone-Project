696fe82d0877630be26284920b5387299024a353
Daniel Pawlak <daniel.pawlak.und@gmail.com>
2020-10-13 08:56:30 -0700

    Updates

---
+++ precache-manifest.eebb898a3466b8db509632b02c2dbdf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bc73bcb8a587fc19534b9bc73fb812a",
+    "url": "/Amazon-Clone-Project/index.html"
+  },
+  {
+    "revision": "e1026461d068e58ed741",
+    "url": "/Amazon-Clone-Project/static/css/main.21d84e04.chunk.css"
+  },
+  {
+    "revision": "7ec53c916a4e6bd26a38",
+    "url": "/Amazon-Clone-Project/static/js/2.5771220a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Amazon-Clone-Project/static/js/2.5771220a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1026461d068e58ed741",
+    "url": "/Amazon-Clone-Project/static/js/main.e069bd77.chunk.js"
+  },
+  {
+    "revision": "bbf759124ee553371553",
+    "url": "/Amazon-Clone-Project/static/js/runtime-main.a6bf3cb1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dpawlak/Amazon-Clone-Project/precache-manifest.f93f72d88ed571ad2bd7a36364bd098f.js"
+  "/Amazon-Clone-Project/precache-manifest.eebb898a3466b8db509632b02c2dbdf9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dpawlak/Amazon-Clone-Project/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Amazon-Clone-Project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
